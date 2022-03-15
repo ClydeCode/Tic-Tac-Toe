f6849a6699ee48e9e0626843edd8d2de27656946
Clyde <noneclyde@gmail.com>
2022-03-15 15:23:04 +0200

    Add entryWindow and more content

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const squares = document.querySelectorAll('.column');
-const person1 = document.querySelector('#player1');
-const person2 = document.querySelector('#player2');
+const person1 = document.querySelector('#person1');
+const person2 = document.querySelector('#person2');
+const button = document.querySelector('#start-button')
 
 const gameboard = (() => {
     let array = [
@@ -29,9 +30,24 @@ const player = (name, symbol) => {
     };
 };
 
-
 const DOM = (() => {
+    const entryWindow = document.querySelector('.first-wrapper');
+    const window = document.querySelector('.second-wrapper');
+    const playerNick1 = document.getElementById('player1');
+    const playerNick2 = document.getElementById('player2');
+
     const persons = [person1, person2];
+
+    const showGameWindow = () => {
+        window.style.visibility = 'visible';
+        entryWindow.remove();
+    };
+
+    const getInputValue = () => {
+         game.players[0] = player(playerNick1.value, 'X');
+         game.players[1] = player(playerNick2.value, '0');
+         console.log(playerNick1.value)
+    };
     
     const setNicknames = () => {
         index = 0;
@@ -47,19 +63,27 @@ const DOM = (() => {
             };
         });
     };
-    return {renderTable, setNicknames};
+    return {renderTable, setNicknames, showGameWindow, getInputValue};
 })();
 
-const player1 = player('Jeff', 'X');
-const player2 = player('Mark', '0');
+// const player1 = player('Jeff', 'X');
+// const player2 = player('Mark', '0');
 
 const game = (() => {
+    let player1;
+    let player2;
+
     const players = [player1, player2];
     let roundIsOver = false;
     let steps = 0;
     
     const nextTurn = () => steps++ % 2;
     
+    // const createPlayers = (name1, name2) => {
+    //     players[0] = player(name1, 'X');
+    //     players[1] = player(name2, '0');
+    // }
+
     const playerMark = () => {
         const player = players[nextTurn()];
         player.moves++;
@@ -71,10 +95,9 @@ const game = (() => {
     const mark = (item) => {
         const id = item.id.split(' ');
         if (!isAlreadyMarked(item) && !roundIsOver) gameboard.check(id[0], id[1], playerMark());
-        DOM.renderTable();
     };
     
-    const checkWin = () => {
+    const checkCondition = () => {
         const array = gameboard.viewArray;
         
         for (let n = 0; n <= 2; n++) {
@@ -91,19 +114,26 @@ const game = (() => {
                 return '0';
             };
         };
+        if (players[0].moves + players[1].moves === 9) return 'tie';
+
         return false;
     };
     
     const checkWinner = () => {
-        if (checkWin() && !roundIsOver) {
+        if (checkCondition() === 'X' || '0' && !roundIsOver) {
             players.forEach(player => {
-                if (player.symbol === checkWin()) { 
+                if (player.symbol === checkCondition()) { 
                     player.score++;
                     roundIsOver = true;
                     return console.log(`${player.name} won!`);
                 };
             });
-        }};
+        }
+        if (checkCondition() === 'tie' && !roundIsOver) {
+            roundIsOver = true;
+            return console.log('tie');
+        }
+    };
         
     const restart = () => {
         for (let row = 0; row <= 2; row++) 
@@ -121,16 +151,17 @@ const game = (() => {
     })();
             
     const beginGame = () => {
+        DOM.showGameWindow();
         DOM.setNicknames();
         squares.forEach(square => {
             square.addEventListener('click', () => {
                 game.mark(square);
+                DOM.renderTable();
                 game.checkWinner();
         });
     });
 };
 
-beginGame();
-    
+button.addEventListener('click', () => beginGame());    
     
     
